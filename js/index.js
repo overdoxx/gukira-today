214f6d398cf19631bd3890332e2dbc658e3404a0
Gustavo <gustavomontiselli@gmail.com>
2022-04-05 17:39:12 -0300

    aaaa

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 setInterval( ()=>{
     let now =  new Date()
-    let past = new Date('2022-03-26T00:00:00')
+    let past = new Date('2022-03-26T17:00:00')
     let diff = Math.abs(now.getTime() - past.getTime())
     let days = Math.ceil(diff / (1000 * 60 * 60 * 24) - 1);
+
     let sec = ((now.getTime() - past.getTime()) / 1000).toString().slice(0, -3)
     let anodiff = now.getFullYear() - past.getFullYear()
     let mesdiff = now.getMonth() - past.getMonth()
@@ -16,3 +17,4 @@ setInterval( ()=>{
     if(mesdiff != 0 && anodiff != 0){document.getElementById("data").innerText = `\n${anodiff} ${anodiff == 1 ? "ano" : "anos"} e ${mesdiff} ${mesdiff == 1 ? "mes" : "meses"}`}}
 
     },1000)
+    
