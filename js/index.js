5c812614deccd46b6477d3e6e8d0bf535ea7252b
Gustavo <gustavomontiselli@gmail.com>
2022-04-05 00:52:09 -0300

    primeiro

---
+++ js/index.js
@@ -0,0 +1,28 @@
+async function cont(){
+
+    setInterval( ()=>{
+    let now =  new Date()
+    let past = new Date('2022-03-26T00:00:00')
+    let diff = Math.abs(now.getTime() - past.getTime())
+    let days = Math.ceil(diff / (1000 * 60 * 60 * 24) - 1);
+    let sec = (now.getTime() - past.getTime()) / 1000
+    let anodiff = now.getFullYear() - past.getFullYear()
+    let mesdiff = now.getMonth() - past.getMonth()
+    console.log(mesdiff)
+    document.getElementById("data").innerText = `${days.toString()} dias`
+    document.getElementById('sec').innerText = new Intl.NumberFormat('pt-BR', { style: 'decimal' }).format(sec).slice(0, -4)
+    document.getElementById("interface").style.display = "inline";
+
+    if(now.getDate() == past.getDate()){
+        document.getElementById("data").innerText = `${mesdiff} ${mesdiff == 1 ? "mes" : "meses"}`
+    if(mesdiff == 0){
+        document.getElementById("data").innerText = `${anodiff} ${anodiff == 1 ? "ano" : "anos"}`
+    }
+    if(mesdiff != 0){
+        document.getElementById("data").innerText = `\n${anodiff} ${anodiff == 1 ? "ano" : "anos"} e ${mesdiff} ${mesdiff == 1 ? "mes" : "meses"}`
+    }
+    }
+
+    },1000)
+    }
+    cont()
